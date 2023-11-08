542e56df29f2a816d700eb282ebf7e62766bb0aa
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-08 08:53:54 +0000

    Start at zero

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -29,7 +29,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.expressionBox = new System.Windows.Forms.TextBox();
             this.num1 = new System.Windows.Forms.Button();
             this.num2 = new System.Windows.Forms.Button();
             this.numb3 = new System.Windows.Forms.Button();
@@ -46,230 +45,157 @@
             this.butDeleteAll = new System.Windows.Forms.Button();
             this.butDeleteCurrently = new System.Windows.Forms.Button();
             this.butProcent = new System.Windows.Forms.Button();
-            this.numb0 = new System.Windows.Forms.Button();
-            this.butRemoSym = new System.Windows.Forms.Button();
-            this.butKoma = new System.Windows.Forms.Button();
-            this.butEqual = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // expressionBox
-            // 
-            this.expressionBox.Location = new System.Drawing.Point(12, 12);
-            this.expressionBox.Multiline = true;
-            this.expressionBox.Name = "expressionBox";
-            this.expressionBox.Size = new System.Drawing.Size(405, 170);
-            this.expressionBox.TabIndex = 0;
-            this.expressionBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // num1
             // 
-            this.num1.Location = new System.Drawing.Point(25, 467);
+            this.num1.Location = new System.Drawing.Point(22, 395);
             this.num1.Name = "num1";
             this.num1.Size = new System.Drawing.Size(90, 76);
             this.num1.TabIndex = 1;
             this.num1.Text = "1";
             this.num1.UseVisualStyleBackColor = true;
-            this.num1.Click += new System.EventHandler(this.num1_Click);
             // 
             // num2
             // 
-            this.num2.Location = new System.Drawing.Point(121, 467);
+            this.num2.Location = new System.Drawing.Point(118, 395);
             this.num2.Name = "num2";
             this.num2.Size = new System.Drawing.Size(90, 76);
             this.num2.TabIndex = 2;
             this.num2.Text = "2";
             this.num2.UseVisualStyleBackColor = true;
-            this.num2.Click += new System.EventHandler(this.num2_Click);
             // 
             // numb3
             // 
-            this.numb3.Location = new System.Drawing.Point(217, 467);
+            this.numb3.Location = new System.Drawing.Point(214, 395);
             this.numb3.Name = "numb3";
             this.numb3.Size = new System.Drawing.Size(90, 76);
             this.numb3.TabIndex = 3;
             this.numb3.Text = "3";
             this.numb3.UseVisualStyleBackColor = true;
-            this.numb3.Click += new System.EventHandler(this.numb3_Click);
             // 
             // butPlus
             // 
-            this.butPlus.Location = new System.Drawing.Point(313, 467);
+            this.butPlus.Location = new System.Drawing.Point(310, 395);
             this.butPlus.Name = "butPlus";
             this.butPlus.Size = new System.Drawing.Size(90, 76);
             this.butPlus.TabIndex = 4;
             this.butPlus.Text = "+";
             this.butPlus.UseVisualStyleBackColor = true;
-            this.butPlus.Click += new System.EventHandler(this.butPlus_Click);
             // 
             // butMinus
             // 
-            this.butMinus.Location = new System.Drawing.Point(313, 385);
+            this.butMinus.Location = new System.Drawing.Point(310, 313);
             this.butMinus.Name = "butMinus";
             this.butMinus.Size = new System.Drawing.Size(90, 76);
             this.butMinus.TabIndex = 8;
             this.butMinus.Text = "-";
             this.butMinus.UseVisualStyleBackColor = true;
-            this.butMinus.Click += new System.EventHandler(this.butMinus_Click);
             // 
             // numb6
             // 
-            this.numb6.Location = new System.Drawing.Point(217, 385);
+            this.numb6.Location = new System.Drawing.Point(214, 313);
             this.numb6.Name = "numb6";
             this.numb6.Size = new System.Drawing.Size(90, 76);
             this.numb6.TabIndex = 7;
             this.numb6.Text = "6";
             this.numb6.UseVisualStyleBackColor = true;
-            this.numb6.Click += new System.EventHandler(this.numb6_Click);
             // 
             // numb5
             // 
-            this.numb5.Location = new System.Drawing.Point(121, 385);
+            this.numb5.Location = new System.Drawing.Point(118, 313);
             this.numb5.Name = "numb5";
             this.numb5.Size = new System.Drawing.Size(90, 76);
             this.numb5.TabIndex = 6;
             this.numb5.Text = "5";
             this.numb5.UseVisualStyleBackColor = true;
-            this.numb5.Click += new System.EventHandler(this.numb5_Click);
             // 
             // numb4
             // 
-            this.numb4.Location = new System.Drawing.Point(25, 385);
+            this.numb4.Location = new System.Drawing.Point(22, 313);
             this.numb4.Name = "numb4";
             this.numb4.Size = new System.Drawing.Size(90, 76);
             this.numb4.TabIndex = 5;
             this.numb4.Text = "4";
             this.numb4.UseVisualStyleBackColor = true;
-            this.numb4.Click += new System.EventHandler(this.numb4_Click);
             // 
             // butMultiple
             // 
-            this.butMultiple.Location = new System.Drawing.Point(313, 303);
+            this.butMultiple.Location = new System.Drawing.Point(310, 231);
             this.butMultiple.Name = "butMultiple";
             this.butMultiple.Size = new System.Drawing.Size(90, 76);
             this.butMultiple.TabIndex = 12;
             this.butMultiple.Text = "x";
             this.butMultiple.UseVisualStyleBackColor = true;
-            this.butMultiple.Click += new System.EventHandler(this.butMultiple_Click);
             // 
             // numb9
             // 
-            this.numb9.Location = new System.Drawing.Point(217, 303);
+            this.numb9.Location = new System.Drawing.Point(214, 231);
             this.numb9.Name = "numb9";
             this.numb9.Size = new System.Drawing.Size(90, 76);
             this.numb9.TabIndex = 11;
             this.numb9.Text = "9";
             this.numb9.UseVisualStyleBackColor = true;
-            this.numb9.Click += new System.EventHandler(this.numb9_Click);
             // 
             // numb8
             // 
-            this.numb8.Location = new System.Drawing.Point(121, 303);
+            this.numb8.Location = new System.Drawing.Point(118, 231);
             this.numb8.Name = "numb8";
             this.numb8.Size = new System.Drawing.Size(90, 76);
             this.numb8.TabIndex = 10;
             this.numb8.Text = "8";
             this.numb8.UseVisualStyleBackColor = true;
-            this.numb8.Click += new System.EventHandler(this.numb8_Click);
             // 
             // numb7
             // 
-            this.numb7.Location = new System.Drawing.Point(25, 303);
+            this.numb7.Location = new System.Drawing.Point(22, 231);
             this.numb7.Name = "numb7";
             this.numb7.Size = new System.Drawing.Size(90, 76);
             this.numb7.TabIndex = 9;
             this.numb7.Text = "7";
             this.numb7.UseVisualStyleBackColor = true;
-            this.numb7.Click += new System.EventHandler(this.numb7_Click);
             // 
             // numbDivis
             // 
-            this.numbDivis.Location = new System.Drawing.Point(217, 221);
+            this.numbDivis.Location = new System.Drawing.Point(214, 149);
             this.numbDivis.Name = "numbDivis";
             this.numbDivis.Size = new System.Drawing.Size(90, 76);
             this.numbDivis.TabIndex = 16;
             this.numbDivis.Text = "÷";
             this.numbDivis.UseVisualStyleBackColor = true;
-            this.numbDivis.Click += new System.EventHandler(this.numbDivis_Click);
             // 
             // butDeleteAll
             // 
-            this.butDeleteAll.Location = new System.Drawing.Point(121, 221);
+            this.butDeleteAll.Location = new System.Drawing.Point(118, 149);
             this.butDeleteAll.Name = "butDeleteAll";
             this.butDeleteAll.Size = new System.Drawing.Size(90, 76);
             this.butDeleteAll.TabIndex = 15;
             this.butDeleteAll.Text = "C";
             this.butDeleteAll.UseVisualStyleBackColor = true;
-            this.butDeleteAll.Click += new System.EventHandler(this.butDeleteAll_Click);
             // 
             // butDeleteCurrently
             // 
-            this.butDeleteCurrently.Location = new System.Drawing.Point(25, 221);
+            this.butDeleteCurrently.Location = new System.Drawing.Point(22, 149);
             this.butDeleteCurrently.Name = "butDeleteCurrently";
             this.butDeleteCurrently.Size = new System.Drawing.Size(90, 76);
             this.butDeleteCurrently.TabIndex = 14;
             this.butDeleteCurrently.Text = "CE";
             this.butDeleteCurrently.UseVisualStyleBackColor = true;
-            this.butDeleteCurrently.Click += new System.EventHandler(this.butDeleteCurrently_Click);
             // 
             // butProcent
             // 
-            this.butProcent.Location = new System.Drawing.Point(25, 549);
+            this.butProcent.Location = new System.Drawing.Point(22, 477);
             this.butProcent.Name = "butProcent";
             this.butProcent.Size = new System.Drawing.Size(90, 76);
             this.butProcent.TabIndex = 13;
             this.butProcent.Text = "%";
             this.butProcent.UseVisualStyleBackColor = true;
-            this.butProcent.Click += new System.EventHandler(this.butProcent_Click);
-            // 
-            // numb0
-            // 
-            this.numb0.Location = new System.Drawing.Point(121, 549);
-            this.numb0.Name = "numb0";
-            this.numb0.Size = new System.Drawing.Size(90, 76);
-            this.numb0.TabIndex = 17;
-            this.numb0.Text = "0";
-            this.numb0.UseVisualStyleBackColor = true;
-            this.numb0.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // butRemoSym
-            // 
-            this.butRemoSym.Location = new System.Drawing.Point(313, 221);
-            this.butRemoSym.Name = "butRemoSym";
-            this.butRemoSym.Size = new System.Drawing.Size(90, 76);
-            this.butRemoSym.TabIndex = 18;
-            this.butRemoSym.Text = "⌫";
-            this.butRemoSym.UseVisualStyleBackColor = true;
-            this.butRemoSym.Click += new System.EventHandler(this.butRemoSym_Click);
-            // 
-            // butKoma
-            // 
-            this.butKoma.Location = new System.Drawing.Point(217, 549);
-            this.butKoma.Name = "butKoma";
-            this.butKoma.Size = new System.Drawing.Size(90, 76);
-            this.butKoma.TabIndex = 19;
-            this.butKoma.Text = ",";
-            this.butKoma.UseVisualStyleBackColor = true;
-            this.butKoma.Click += new System.EventHandler(this.butKoma_Click);
-            // 
-            // butEqual
-            // 
-            this.butEqual.Location = new System.Drawing.Point(313, 549);
-            this.butEqual.Name = "butEqual";
-            this.butEqual.Size = new System.Drawing.Size(90, 76);
-            this.butEqual.TabIndex = 20;
-            this.butEqual.Text = "=";
-            this.butEqual.UseVisualStyleBackColor = true;
-            this.butEqual.Click += new System.EventHandler(this.butEqual_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(426, 661);
-            this.Controls.Add(this.butEqual);
-            this.Controls.Add(this.butKoma);
-            this.Controls.Add(this.butRemoSym);
-            this.Controls.Add(this.numb0);
+            this.ClientSize = new System.Drawing.Size(426, 584);
             this.Controls.Add(this.numbDivis);
             this.Controls.Add(this.butDeleteAll);
             this.Controls.Add(this.butDeleteCurrently);
@@ -286,16 +212,13 @@
             this.Controls.Add(this.numb3);
             this.Controls.Add(this.num2);
             this.Controls.Add(this.num1);
-            this.Controls.Add(this.expressionBox);
             this.Name = "Form1";
             this.Text = "Calculator";
             this.ResumeLayout(false);
-            this.PerformLayout();
         }
 
         #endregion
 
-        private System.Windows.Forms.TextBox expressionBox;
         private System.Windows.Forms.Button num1;
         private System.Windows.Forms.Button num2;
         private System.Windows.Forms.Button numb3;
@@ -312,9 +235,5 @@
         private System.Windows.Forms.Button butDeleteAll;
         private System.Windows.Forms.Button butDeleteCurrently;
         private System.Windows.Forms.Button butProcent;
-        private System.Windows.Forms.Button numb0;
-        private System.Windows.Forms.Button butRemoSym;
-        private System.Windows.Forms.Button butKoma;
-        private System.Windows.Forms.Button butEqual;
     }
 }
