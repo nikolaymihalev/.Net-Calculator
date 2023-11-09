54ffae0bc16bb560166f413f3b23811cff0c73dc
Nikolay Mihalev <nikimihalev1213@gmail.com>
2023-11-09 08:12:46 +0000

    Design

--- Calculator/Calculator/Form1.Designer.cs
+++ Calculator/Calculator/Form1.Designer.cs
@@ -53,225 +53,285 @@
             // 
             // num1
             // 
+            this.num1.BackColor = System.Drawing.Color.Gray;
+            this.num1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.num1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.num1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.num1.Location = new System.Drawing.Point(22, 395);
             this.num1.Name = "num1";
             this.num1.Size = new System.Drawing.Size(90, 76);
             this.num1.TabIndex = 1;
             this.num1.Text = "1";
-            this.num1.UseVisualStyleBackColor = true;
+            this.num1.UseVisualStyleBackColor = false;
             this.num1.Click += new System.EventHandler(this.button_click);
             // 
             // num2
             // 
+            this.num2.BackColor = System.Drawing.Color.Gray;
+            this.num2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.num2.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.num2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.num2.Location = new System.Drawing.Point(118, 395);
             this.num2.Name = "num2";
             this.num2.Size = new System.Drawing.Size(90, 76);
             this.num2.TabIndex = 2;
             this.num2.Text = "2";
-            this.num2.UseVisualStyleBackColor = true;
+            this.num2.UseVisualStyleBackColor = false;
             this.num2.Click += new System.EventHandler(this.button_click);
             // 
             // numb3
             // 
+            this.numb3.BackColor = System.Drawing.Color.Gray;
+            this.numb3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb3.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb3.Location = new System.Drawing.Point(214, 395);
             this.numb3.Name = "numb3";
             this.numb3.Size = new System.Drawing.Size(90, 76);
             this.numb3.TabIndex = 3;
             this.numb3.Text = "3";
-            this.numb3.UseVisualStyleBackColor = true;
+            this.numb3.UseVisualStyleBackColor = false;
             this.numb3.Click += new System.EventHandler(this.button_click);
             // 
             // butPlus
             // 
+            this.butPlus.BackColor = System.Drawing.Color.Orange;
+            this.butPlus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butPlus.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.butPlus.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.butPlus.Location = new System.Drawing.Point(310, 313);
             this.butPlus.Name = "butPlus";
             this.butPlus.Size = new System.Drawing.Size(90, 76);
             this.butPlus.TabIndex = 4;
             this.butPlus.Text = "+";
-            this.butPlus.UseVisualStyleBackColor = true;
+            this.butPlus.UseVisualStyleBackColor = false;
             this.butPlus.Click += new System.EventHandler(this.operator_click);
             // 
             // butMinus
             // 
+            this.butMinus.BackColor = System.Drawing.Color.Orange;
+            this.butMinus.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butMinus.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.butMinus.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.butMinus.Location = new System.Drawing.Point(310, 231);
             this.butMinus.Name = "butMinus";
             this.butMinus.Size = new System.Drawing.Size(90, 76);
             this.butMinus.TabIndex = 8;
             this.butMinus.Text = "-";
-            this.butMinus.UseVisualStyleBackColor = true;
+            this.butMinus.UseVisualStyleBackColor = false;
             this.butMinus.Click += new System.EventHandler(this.operator_click);
             // 
             // numb6
             // 
+            this.numb6.BackColor = System.Drawing.Color.Gray;
+            this.numb6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb6.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb6.Location = new System.Drawing.Point(214, 313);
             this.numb6.Name = "numb6";
             this.numb6.Size = new System.Drawing.Size(90, 76);
             this.numb6.TabIndex = 7;
             this.numb6.Text = "6";
-            this.numb6.UseVisualStyleBackColor = true;
+            this.numb6.UseVisualStyleBackColor = false;
             this.numb6.Click += new System.EventHandler(this.button_click);
             // 
             // numb5
             // 
+            this.numb5.BackColor = System.Drawing.Color.Gray;
+            this.numb5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb5.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb5.Location = new System.Drawing.Point(118, 313);
             this.numb5.Name = "numb5";
             this.numb5.Size = new System.Drawing.Size(90, 76);
             this.numb5.TabIndex = 6;
             this.numb5.Text = "5";
-            this.numb5.UseVisualStyleBackColor = true;
+            this.numb5.UseVisualStyleBackColor = false;
             this.numb5.Click += new System.EventHandler(this.button_click);
             // 
             // numb4
             // 
+            this.numb4.BackColor = System.Drawing.Color.Gray;
+            this.numb4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb4.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb4.Location = new System.Drawing.Point(22, 313);
             this.numb4.Name = "numb4";
             this.numb4.Size = new System.Drawing.Size(90, 76);
             this.numb4.TabIndex = 5;
             this.numb4.Text = "4";
-            this.numb4.UseVisualStyleBackColor = true;
+            this.numb4.UseVisualStyleBackColor = false;
             this.numb4.Click += new System.EventHandler(this.button_click);
             // 
             // butMultiple
             // 
+            this.butMultiple.BackColor = System.Drawing.Color.Orange;
+            this.butMultiple.FlatAppearance.BorderSize = 0;
+            this.butMultiple.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butMultiple.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.butMultiple.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.butMultiple.Location = new System.Drawing.Point(310, 149);
             this.butMultiple.Name = "butMultiple";
             this.butMultiple.Size = new System.Drawing.Size(90, 76);
             this.butMultiple.TabIndex = 12;
             this.butMultiple.Text = "*";
-            this.butMultiple.UseVisualStyleBackColor = true;
+            this.butMultiple.UseVisualStyleBackColor = false;
             this.butMultiple.Click += new System.EventHandler(this.operator_click);
             // 
             // numb9
             // 
+            this.numb9.BackColor = System.Drawing.Color.Gray;
+            this.numb9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb9.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb9.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb9.Location = new System.Drawing.Point(214, 231);
             this.numb9.Name = "numb9";
             this.numb9.Size = new System.Drawing.Size(90, 76);
             this.numb9.TabIndex = 11;
             this.numb9.Text = "9";
-            this.numb9.UseVisualStyleBackColor = true;
+            this.numb9.UseVisualStyleBackColor = false;
             this.numb9.Click += new System.EventHandler(this.button_click);
             // 
             // numb8
             // 
+            this.numb8.BackColor = System.Drawing.Color.Gray;
+            this.numb8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb8.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb8.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb8.Location = new System.Drawing.Point(118, 231);
             this.numb8.Name = "numb8";
             this.numb8.Size = new System.Drawing.Size(90, 76);
             this.numb8.TabIndex = 10;
             this.numb8.Text = "8";
-            this.numb8.UseVisualStyleBackColor = true;
+            this.numb8.UseVisualStyleBackColor = false;
             this.numb8.Click += new System.EventHandler(this.button_click);
             // 
             // numb7
             // 
+            this.numb7.BackColor = System.Drawing.Color.Gray;
+            this.numb7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numb7.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.numb7.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.numb7.Location = new System.Drawing.Point(22, 231);
             this.numb7.Name = "numb7";
             this.numb7.Size = new System.Drawing.Size(90, 76);
             this.numb7.TabIndex = 9;
             this.numb7.Text = "7";
-            this.numb7.UseVisualStyleBackColor = true;
+            this.numb7.UseVisualStyleBackColor = false;
             this.numb7.Click += new System.EventHandler(this.button_click);
             // 
             // numbDivis
             // 
+            this.numbDivis.BackColor = System.Drawing.Color.Silver;
+            this.numbDivis.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.numbDivis.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
             this.numbDivis.Location = new System.Drawing.Point(214, 149);
             this.numbDivis.Name = "numbDivis";
             this.numbDivis.Size = new System.Drawing.Size(90, 76);
             this.numbDivis.TabIndex = 16;
             this.numbDivis.Text = "/";
-            this.numbDivis.UseVisualStyleBackColor = true;
+            this.numbDivis.UseVisualStyleBackColor = false;
             this.numbDivis.Click += new System.EventHandler(this.operator_click);
             // 
             // butDeleteAll
             // 
+            this.butDeleteAll.BackColor = System.Drawing.Color.Silver;
+            this.butDeleteAll.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butDeleteAll.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
             this.butDeleteAll.Location = new System.Drawing.Point(118, 149);
             this.butDeleteAll.Name = "butDeleteAll";
             this.butDeleteAll.Size = new System.Drawing.Size(90, 76);
             this.butDeleteAll.TabIndex = 15;
             this.butDeleteAll.Text = "C";
-            this.butDeleteAll.UseVisualStyleBackColor = true;
+            this.butDeleteAll.UseVisualStyleBackColor = false;
             this.butDeleteAll.Click += new System.EventHandler(this.butDeleteAll_Click);
             // 
             // butDeleteCurrently
             // 
+            this.butDeleteCurrently.BackColor = System.Drawing.Color.Silver;
+            this.butDeleteCurrently.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butDeleteCurrently.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.butDeleteCurrently.Location = new System.Drawing.Point(22, 149);
             this.butDeleteCurrently.Name = "butDeleteCurrently";
             this.butDeleteCurrently.Size = new System.Drawing.Size(90, 76);
             this.butDeleteCurrently.TabIndex = 14;
             this.butDeleteCurrently.Text = "CE";
-            this.butDeleteCurrently.UseVisualStyleBackColor = true;
+            this.butDeleteCurrently.UseVisualStyleBackColor = false;
             this.butDeleteCurrently.Click += new System.EventHandler(this.butDeleteCurrently_Click);
             // 
             // labOperation
             // 
             this.labOperation.AutoSize = true;
-            this.labOperation.Location = new System.Drawing.Point(19, 26);
+            this.labOperation.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labOperation.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.labOperation.Location = new System.Drawing.Point(324, 45);
             this.labOperation.Name = "labOperation";
-            this.labOperation.Size = new System.Drawing.Size(0, 13);
+            this.labOperation.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.labOperation.Size = new System.Drawing.Size(0, 22);
             this.labOperation.TabIndex = 17;
+            this.labOperation.TextAlign = System.Drawing.ContentAlignment.TopRight;
             // 
             // resultBox
             // 
-            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F);
+            this.resultBox.BackColor = System.Drawing.SystemColors.Desktop;
+            this.resultBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.resultBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 35F);
+            this.resultBox.ForeColor = System.Drawing.SystemColors.Window;
             this.resultBox.Location = new System.Drawing.Point(22, 79);
             this.resultBox.Multiline = true;
             this.resultBox.Name = "resultBox";
-            this.resultBox.Size = new System.Drawing.Size(392, 45);
+            this.resultBox.Size = new System.Drawing.Size(392, 64);
             this.resultBox.TabIndex = 18;
             this.resultBox.Text = "0";
             this.resultBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // butZero
             // 
+            this.butZero.BackColor = System.Drawing.Color.Gray;
+            this.butZero.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butZero.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.butZero.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.butZero.Location = new System.Drawing.Point(22, 477);
             this.butZero.Name = "butZero";
             this.butZero.Size = new System.Drawing.Size(186, 76);
             this.butZero.TabIndex = 19;
             this.butZero.Text = "0";
-            this.butZero.UseVisualStyleBackColor = true;
+            this.butZero.UseVisualStyleBackColor = false;
             this.butZero.Click += new System.EventHandler(this.button_click);
             // 
             // butKoma
             // 
+            this.butKoma.BackColor = System.Drawing.Color.Gray;
+            this.butKoma.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butKoma.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.butKoma.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.butKoma.Location = new System.Drawing.Point(214, 477);
             this.butKoma.Name = "butKoma";
             this.butKoma.Size = new System.Drawing.Size(90, 76);
             this.butKoma.TabIndex = 20;
             this.butKoma.Text = ",";
-            this.butKoma.UseVisualStyleBackColor = true;
+            this.butKoma.UseVisualStyleBackColor = false;
             this.butKoma.Click += new System.EventHandler(this.button_click);
             // 
             // butEqual
             // 
+            this.butEqual.BackColor = System.Drawing.Color.Orange;
+            this.butEqual.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.butEqual.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold);
+            this.butEqual.ForeColor = System.Drawing.SystemColors.ControlLightLight;
             this.butEqual.Location = new System.Drawing.Point(310, 395);
             this.butEqual.Name = "butEqual";
             this.butEqual.Size = new System.Drawing.Size(90, 158);
             this.butEqual.TabIndex = 21;
             this.butEqual.Text = "=";
-            this.butEqual.UseVisualStyleBackColor = true;
+            this.butEqual.UseVisualStyleBackColor = false;
             this.butEqual.Click += new System.EventHandler(this.butEqual_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
             this.ClientSize = new System.Drawing.Size(426, 584);
             this.Controls.Add(this.butEqual);
             this.Controls.Add(this.butKoma);
